c6b8a6c3697515bef4ba98d8acda51508deb0965
MPythonS <starkus.mantas@gmail.com>
2023-06-08 21:33:23 +0300

    os.getenv

--- conference_app/conference_app/settings.py
+++ conference_app/conference_app/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
-
+SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
+# os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.getenv("SECRET_KEY")=="True" else False
+DEBUG = True
 # True if os.getenv("SECRET_KEY")=="True" else False
 
 ALLOWED_HOSTS = ["*"]
